16e777f71ac4fe0855826eb39827b545e32f59d7
Fabio A Falavinha <fabio.falavinha@interplayers.com.br>
2021-06-10 10:44:36 -0300

    initial setup

---
+++ ECS.Workshop.DesignCleanCode.Demo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECS.Workshop.DesignCleanCode.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Press <ENTER> to exit application");
+            Console.ReadKey();
+        }
+    }
+}
